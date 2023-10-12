e77d4815b87e84cf070e5a8ee0a97c0d08fd87db
Vaibhav Bhardwaj <vaibhav07351@gmail.com>
2023-10-12 12:04:57 +0530

    first commit

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Policy = () => {
+  return (
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy1</p>
+          <p>add privacy policy2</p>
+          <p>add privacy policy3</p>
+          <p>add privacy policy4</p>
+          <p>add privacy policy5</p>
+          <p>add privacy policy6</p>
+          <p>add privacy policy7</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
